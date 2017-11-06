542d0ec3c90792d876da4dc568de0ce113c85d4a
Nathalia de Luccas Filopoulos <nathiluccas@gmail.com>
2017-11-06 11:42:32 +0000

    rails new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+def ask
+end
+
+def answer
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  Prefix Verb URI Pattern       Controller#Action
+     ask GET  /ask(.:format)    questions#ask
+  answer GET  /answer(.:format) questions#answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
